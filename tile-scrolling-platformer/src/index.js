5142a75ac389973ea2fecfe64551cc9214fc0de6
willi8leu <118115171+willi8leu@users.noreply.github.com>
2023-02-06 21:05:38 -0500

    0

--- tile-scrolling-platformer/src/index.js
+++ tile-scrolling-platformer/src/index.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector('#canvas'),
       ctx = canvas.getContext('2d'),
+      CANVAS_WIDTH = canvas.width = innerWidth,
+      CANVAS_HEIGHT = canvas.height = innerHeight,
       tiles = document.querySelector('#tiles'),
       characters = document.querySelector('#characters'),
       background = document.querySelector('#background'),
