0aedf9b03cda0dc090547d0d033b4b239d367a13
willi8leu <willi8leu@proton.me>
2023-02-08 02:26:26 +0000

    0

--- tile-scrolling-platformer/src/index.js
+++ tile-scrolling-platformer/src/index.js
@@ -3,17 +3,35 @@ const canvas = document.querySelector('#canvas'),
       CANVAS_WIDTH = canvas.width = innerWidth,
       CANVAS_HEIGHT = canvas.height = innerHeight,
       tiles = document.querySelector('#tiles'),
-      characters = document.querySelector('#characters'),
-      background = document.querySelector('#background'),
-      div = document.querySelector('#div');
+      div = document.querySelector('#div'),
+      TILE_WIDTH = 16,
+      TILE_HEIGHT = 16,
+      DRAW_TILE_WIDTH = 32,
+      DRAW_TILE_HEIGHT = 32,
+      TILEMAP_WIDTH = 20,
+      TILEMAP_HEIGHT = 20,
+      GRID_WIDTH = 20,
+      GRID_HEIGHT = 20;
 
-onload = function gameLoop() {
-    for (let y=0; y<CANVAS_HEIGHT/18; y++) {
-        for (let x=0; x<CANVAS_WIDTH/18; x++) {
-            randX = Math.floor(Math.random()*20)*18;
-            randY = Math.floor(Math.random()*9)*18;
-            ctx.drawImage(tilemap,randX,randY,18,18,x*18,y*18,18,18);
-        }
-    }
+let gridmap = new Array(GRID_HEIGHT).fill();
+gridmap = gridmap.map(() => new Array(GRID_WIDTH).fill());
+gridmap = gridmap.map(row => row.map(el => ~~(Math.random()*TILEMAP_WIDTH*TILEMAP_HEIGHT)));
+div.innerHTML = JSON.stringify(gridmap);
+
+function gameLoop() {
+    drawCanvas();
     //this.requestAnimationFrame(gameLoop);
 }
+
+function drawCanvas() {
+    for (let y=0; y<GRID_HEIGHT; y++) {
+        for (let x=0; x<GRID_WIDTH; x++) {
+            ctx.drawImage(tiles,gridmap[y][x]%TILEMAP_WIDTH*TILE_WIDTH,
+                          ~~(gridmap[y][x]/TILEMAP_WIDTH)*TILE_HEIGHT,TILE_WIDTH,TILE_HEIGHT,
+                          x*DRAW_TILE_WIDTH,y*DRAW_TILE_HEIGHT,DRAW_TILE_WIDTH,DRAW_TILE_HEIGHT);
+        }
+    }
+}
+
+onload = () => {gameLoop()};
+//ctx.fillRect(0,0,100,100);
