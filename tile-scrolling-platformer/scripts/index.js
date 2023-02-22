8577fa3d98171822c068b80923574e979bf5a293
willi8leu <118115171+willi8leu@users.noreply.github.com>
2023-02-22 18:26:21 -0500

    Update index.js

--- tile-scrolling-platformer/scripts/index.js
+++ tile-scrolling-platformer/scripts/index.js
@@ -17,15 +17,19 @@ onkeyup = (event) => {_s.keyPresses.splice(_s.keyPresses.indexOf(event.key.toLow
 level.generateLevel();
 _s.camX = _s.DRAW_TILE_WIDTH;
 _s.camY = (_s.GRIDMAP_HEIGHT*_s.DRAW_TILE_HEIGHT)-_s.canvas.height;
+_s.playerX = _s.DRAW_TILE_WIDTH;
+_s.playerY = _s.GRIDMAP_HEIGHT*_s.DRAW_TILE_HEIGHT;
 
 console.log(_s);
-console.log(_s.canvas.width,(_s.GRIDMAP_WIDTH-1)*_s.DRAW_TILE_WIDTH);
+console.log(_s.gridmap);
+console.log(_s.camX,_s.camY);
 
 function gameLoop() {
     _s.ctx.clearRect(0,0,_s.canvas.width,_s.canvas.height);
     level.drawTiles();
+    player.tick();
     //level.camBounds();
-    _s.div.innerHTML = JSON.stringify(_s.keyPresses)+`, ${_s.playerX}, ${_s.playerY}`;
+    _s.div.innerHTML = JSON.stringify(_s.keyPresses)+`, ${Math.trunc(_s.playerX+_s.camX)}, ${Math.trunc(-_s.playerY-_s.camY)}, ${~~(_s.playerX/_s.DRAW_TILE_WIDTH)}, ${~~(_s.playerY/_s.DRAW_TILE_HEIGHT)}, ${_s.gridmap[~~(-_s.playerY/_s.DRAW_TILE_HEIGHT)][~~(-_s.playerX/_s.DRAW_TILE_WIDTH)]}`;
     requestAnimationFrame(gameLoop);
 }
 onload = () => {gameLoop()};
